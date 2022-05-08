3dc04cc3e059ecf102714bf7bd526936784c2618
Yubraj Bhandari <yubraj.bhandari.hero@gmail.com>
2022-05-08 21:26:58 +0545

    Bug Fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,16 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    ssr: false,
+    generate: {
+        crawler: false
+    },
     // modules: ['@nuxt/content'],
-    serverMiddleware: [
-        '~/middleware/redirect.js'
-    ],
+    // serverMiddleware: [
+    //     '~/middleware/redirect.js'
+    // ],
     css: [
-        '@/assets/css/main.css',
+        '~/assets/css/main.css',
     ],
     build: {
         postcss: {
